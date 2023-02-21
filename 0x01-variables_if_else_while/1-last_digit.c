28e2d8184edd8444f92b888dd1805d5199a641bc
GeorgeKodua21 <georgekodua32@gmail.com>
2023-02-21 14:45:25 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: 'output a sentence with puts'
+ * Description: 'check the last digit of a number'
  * Return: Always 0 (success)
  */
 int main(void)
@@ -14,7 +14,7 @@ if ((n % 10) > 5)
 {
 printf("%s %d is %d and is greater than 5\n", c, n, (n % 10));
 }
-else if ((n % 10) < 6 && (n % 10) !=0)
+else if ((n % 10) < 6 && (n % 10) != 0)
 {
 printf("%s %d is %d and is less than 6 and not 0\n", c, n, (n % 10));
 }
