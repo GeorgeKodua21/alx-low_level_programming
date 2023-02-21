3a3cd64c2a17c81d97f85548f0c2b61cecb17e56
GeorgeKodua21 <georgekodua32@gmail.com>
2023-02-21 01:19:53 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'output a sentence with puts'
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building amultilingual puzzle");
+return (0);
+}
