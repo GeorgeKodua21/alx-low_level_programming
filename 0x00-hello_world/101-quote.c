e4cb991f786ea5fd3d4e75767a63487765fcb8c1
GeorgeKodua21 <georgekodua32@gmail.com>
2023-02-21 02:10:07 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char st[] = "2015-10-19";
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, %s\n", st);
+return (1);
+}
