d52b7dfa3f37e49ed72cb6e6b61d7a2c5f566a74
GeorgeKodua21 <georgekodua32@gmail.com>
2023-02-23 07:58:37 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*_abs - returns the absolute of the input
+*@c: it is an intger
+*description: returns the absolute of the input'
+*Return: 0
+*/
+int _abs(int c)
+{
+if (c >= 0)
+{
+return (c);
+}
+return (-c);
+}
