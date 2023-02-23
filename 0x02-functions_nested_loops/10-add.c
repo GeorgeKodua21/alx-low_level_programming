16f24170ff2513cdf94328cc272ec1f3998296b4
GeorgeKodua21 <georgekodua32@gmail.com>
2023-02-23 08:08:46 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*add - a function
+*@a:  an integer
+*@b:  an integer
+*description: prints every minute of the day of Jack Bauer
+*Return: 0
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
