cedd8f678de16ada056954cb49e2fe0cecb8f557
GeorgeKodua21 <georgekodua32@gmail.com>
2023-02-24 07:29:06 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*mul - calculates the products if two numbers
+*@a: it is an intger
+*@b: it is an integer
+*
+*Description: returns the product of two inputs
+*Return: 0
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
