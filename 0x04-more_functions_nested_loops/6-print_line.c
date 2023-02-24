952531e57f82144cd80acbce130d507c00e688e0
GeorgeKodua21 <georgekodua32@gmail.com>
2023-02-24 13:05:47 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*print_line - prints a line
+*@n: it is an intger
+*
+*Description: prints n number of lines
+*Return: 0
+*/
+void print_line(int n)
+{
+int a;
+for (a = 0; a < n; a++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
